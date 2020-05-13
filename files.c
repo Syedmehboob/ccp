7271a6838252d522311827c5d04d09d31a0dc86c
Syedmehboob <65231640+Syedmehboob@users.noreply.github.com>
2020-05-13 11:57:21 +0530

    1BM19CS169

---
+++ files.c
@@ -0,0 +1,30 @@
+/*Write a program to read data from keyboard and
+ write it to a file called fp, read the same data from the fp file and
+ display it on the screen*/
+#include <stdio.h>
+int main()
+{
+  FILE *fp;
+  char name[80];
+  int rollno;
+  puts("Writing data for file");
+  fp=fopen("INPUT.txt","w+");
+  if(fp==NULL)
+  {
+    printf("\n the file could not be opened");
+  }
+  printf("\n enter the name and Roll number\t");
+  scanf("%s",&name);
+  scanf("%d",&rollno);
+  fprintf(fp,"Name=%s\n",name);
+  fprintf(fp,"roll_no=%d",rollno);
+  fclose(fp);
+  puts("Reading from file");
+  fp=fopen("INPUT.txt","a");
+  while(fscanf(fp,"%s",name)!=EOF)
+  {
+     printf("%s\n",name);
+  }
+  fclose(fp);
+  return 0;
+}
